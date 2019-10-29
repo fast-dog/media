95f9b37f1b4e1f0ef0180ffb8a894e5a3a6c33a9
dg <d.g.dev482@gmail.com>
2019-10-29 21:06:50 +0500

    fix fatal error paste listeners

--- src/Listeners/PasteFileElFinder.php
+++ src/Listeners/PasteFileElFinder.php
@@ -45,59 +45,59 @@ class PasteFileElFinder
          */
         $moduleManager = \App::make(ModuleManager::class);
         $module = $moduleManager->getInstance('FastDog\Media\Media');
-        $setting = $module->getModuleSetting();
-
-        if (isset($data->changed) && isset($data->added)) {
-            $volId = '';
-            $dir = '';
-            foreach ($data->changed as $item) {
-                if ($item->mime == 'directory') {
-                    $volId = $item->volumeid;
-                    $dir = str_replace('\\', '/', base64_decode(str_replace($item->volumeid, '', $item->hash)));
-                }
-            }
-            if (isset($data->removed)) {
-                foreach ($data->removed as $hash) {
-                    $check = GalleryItem::where(GalleryItem::HASH, md5($hash))->first();
-                    if (($setting !== null && $setting->history == true) && $check) {
-                        GalleryItemHistory::create([
-                            GalleryItemHistory::PARENT_ID => $check->id,
-                            GalleryItemHistory::TYPE_ID => GalleryItemHistory::ACTION_MOVE,
-                            GalleryItemHistory::DATA => json_encode([
-                                'dir' => $dir,
-                            ]),
-                        ]);
-                    }
-                }
-            }
-            if (isset($data->added)) {
-                foreach ($data->added as $item) {
-                    $check = GalleryItem::where(GalleryItem::HASH, md5($item->hash))->withTrashed()->first();
-                    if ($check) {
-                        $check->restore();
-                    } else {
-                        $file = base64_decode(str_replace($volId, '', $item->hash));
-                        $file = '/upload/' . str_replace('\\', '/', $file);
-                        $path = str_replace($this->request->url(), '', $file);
-                        $check = GalleryItem::create([
-                            GalleryItem::PARENT_TYPE => $this->request->input(GalleryItem::PARENT_TYPE),
-                            GalleryItem::PARENT_ID => $this->request->input(GalleryItem::PARENT_ID),
-                            GalleryItem::PATH => $path,
-                            GalleryItem::DATA => json_encode($item),
-                            GalleryItem::HASH => md5($item->hash),
-                        ]);
-                    }
-                    if (($setting !== null && $setting->history == true)) {
-                        GalleryItemHistory::create([
-                            GalleryItemHistory::PARENT_ID => $check->id,
-                            GalleryItemHistory::TYPE_ID => GalleryItemHistory::ACTION_MOVE,
-                            GalleryItemHistory::DATA => json_encode([
-                                'dir' => $dir,
-                            ]),
-                        ]);
-                    }
-                }
-            }
-        }
+//        $setting = $module->getModuleSetting();// TODO: error
+//
+//        if (isset($data->changed) && isset($data->added)) {
+//            $volId = '';
+//            $dir = '';
+//            foreach ($data->changed as $item) {
+//                if ($item->mime == 'directory') {
+//                    $volId = $item->volumeid;
+//                    $dir = str_replace('\\', '/', base64_decode(str_replace($item->volumeid, '', $item->hash)));
+//                }
+//            }
+//            if (isset($data->removed)) {
+//                foreach ($data->removed as $hash) {
+//                    $check = GalleryItem::where(GalleryItem::HASH, md5($hash))->first();
+//                    if (($setting !== null && $setting->history == true) && $check) {
+//                        GalleryItemHistory::create([
+//                            GalleryItemHistory::PARENT_ID => $check->id,
+//                            GalleryItemHistory::TYPE_ID => GalleryItemHistory::ACTION_MOVE,
+//                            GalleryItemHistory::DATA => json_encode([
+//                                'dir' => $dir,
+//                            ]),
+//                        ]);
+//                    }
+//                }
+//            }
+//            if (isset($data->added)) {
+//                foreach ($data->added as $item) {
+//                    $check = GalleryItem::where(GalleryItem::HASH, md5($item->hash))->withTrashed()->first();
+//                    if ($check) {
+//                        $check->restore();
+//                    } else {
+//                        $file = base64_decode(str_replace($volId, '', $item->hash));
+//                        $file = '/upload/' . str_replace('\\', '/', $file);
+//                        $path = str_replace($this->request->url(), '', $file);
+//                        $check = GalleryItem::create([
+//                            GalleryItem::PARENT_TYPE => $this->request->input(GalleryItem::PARENT_TYPE),
+//                            GalleryItem::PARENT_ID => $this->request->input(GalleryItem::PARENT_ID),
+//                            GalleryItem::PATH => $path,
+//                            GalleryItem::DATA => json_encode($item),
+//                            GalleryItem::HASH => md5($item->hash),
+//                        ]);
+//                    }
+//                    if (($setting !== null && $setting->history == true)) {
+//                        GalleryItemHistory::create([
+//                            GalleryItemHistory::PARENT_ID => $check->id,
+//                            GalleryItemHistory::TYPE_ID => GalleryItemHistory::ACTION_MOVE,
+//                            GalleryItemHistory::DATA => json_encode([
+//                                'dir' => $dir,
+//                            ]),
+//                        ]);
+//                    }
+//                }
+//            }
+//        }
     }
 }
